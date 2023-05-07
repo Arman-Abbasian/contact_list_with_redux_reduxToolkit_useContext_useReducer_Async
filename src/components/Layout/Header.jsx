880bbe92f318dd07a58270633a29601289d7a81e
Arman Abbasian <105098083+Arman-Abbasian@users.noreply.github.com>
2023-05-07 12:25:29 +0430

    some changes

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
         <header className="p-2 bg-primary-4 sticky top-0 right-0 left-0 mb-5 z-10">
             <nav>
                 <ul className="flex justify-start items-center gap-10">
-                    <li><img src={'/images/logo.png'} className='w-20' /></li>
+                    <li><img src={'images/logo.png'} className='w-20' /></li>
                     <ul className="flex-1 flex justify-start items-center gap-6">
                         <li className="hover:bg-primary-1 p-2 rounded-sm transition-all duration-300"><Link to="/">contact List</Link></li>
                         <li className="hover:bg-primary-1 p-2 rounded-sm transition-all duration-300"><Link to="/add">Add contact</Link></li>
